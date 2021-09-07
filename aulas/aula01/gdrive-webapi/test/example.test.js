84660ef15e4f034c86294ec2fa57df44fc8dfe84
Gustavo Silva <gustavos_barros@hotmail.com>
2021-09-06 22:05:10 -0300

    criando proj(checPt em 35min)

---
+++ aulas/aula01/gdrive-webapi/test/example.test.js
@@ -0,0 +1,11 @@
+import {
+    describe,
+    test,
+    expect
+} from '@jest/globals'
+
+describe('@testando', () => {
+    test('#teste teste', () => {
+        expect(true).toBeTruthy()
+    })
+})
